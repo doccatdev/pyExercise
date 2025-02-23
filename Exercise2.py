b27e449634515cb5538dd8d66f0a39154438c025
Muhamad Yusuf Hidayat <40491907+doccatdev@users.noreply.github.com>
2025-02-24 05:58:57 +0700

    Update and rename Ex2.py to Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+def convertToFahrenheit(degreesCelcius):
+    fahrenheit = degreesCelcius * (9/5) + 32
+    return fahrenheit
+
+result = convertToFahrenheit(32)
+print(result)
+
+def convertToCelcius(degreesFahrenheit):
+    return (degreesFahrenheit - 32) * (5/9)
+
+result_2 = convertToCelcius(100)
+print(result_2)
